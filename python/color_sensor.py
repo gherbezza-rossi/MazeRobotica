850971e102f496435875a84218b5cff5bc7493ef
raccococo <sirio.pastori@gmail.com>
2024-04-11 13:27:12 +0200

    dfd

--- python/color_sensor.py
+++ python/color_sensor.py
@@ -12,7 +12,8 @@ import adafruit_tcs34725
 i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 sensor = adafruit_tcs34725.TCS34725(i2c)
-
+sensor.integration_time = 500
+sensor.gain = 60
 # Main loop reading color and printing it every second.
 def read_sensor_data(blu,nero,bianco,specchio):
     lux = sensor.lux
@@ -25,10 +26,24 @@ def read_sensor_data(blu,nero,bianco,specchio):
     elif lux >=(specchio-10) and lux <= (specchio+10):
         print('specchio')
 
-def read_sensor_color_black(nero):
-    lux = sensor.lux
-    if lux >=(nero-2) and lux <= (nero+2):
-        print('nero')
-        return True
-    else:
-        return False
+def read_sensor_color_black(nero, times=10):
+    black_count = 0
+    while True:
+        lux = sensor.lux
+        print(lux)
+        if nero - 2 <= lux <= nero + 2:
+            print('nero')
+            black_count += 1
+            if black_count >= times:
+                return True
+        else:
+            black_count = 0  # Reset the count if color is not black
+        if black_count < times:
+            return False
+
+
+while True:
+    print('Color: ({0}, {1}, {2})'.format(*sensor.color_rgb_bytes))
+    print('Temperature: {0}K'.format(sensor.color_temperature))
+    print('Lux: {0}'.format(sensor.lux))
+    time.sleep(2)
--- python/lettura_encoder.py
+++ python/lettura_encoder.py
@@ -4,22 +4,22 @@ import time
 import numpy as np
 import RPi.GPIO as GPIO
 import board
-#import adafruit_tcs34725
+import adafruit_tcs34725
 
-#i2c = board.I2C()  # uses board.SCL and board.SDA
-#sensor = adafruit_tcs34725.TCS34725(i2c)
-#user_input = input("calibrazione sensore colori, nero (start/no)")
-#if user_input.lower() in ["start", "no"]:
-#    nero = sensor.lux
-#    user_input = input("calibrazione sensore colori, blu (start/no)")
- #   if user_input.lower() in ["start", "no"]:
- #       blu = sensor.lux
- #       user_input = input("calibrazione sensore colori, specchio (start/no)")
- #       if user_input.lower() in ["start", "no"]:
- #           specchio = sensor.lux
- #           user_input = input("calibrazione sensore colori, bianco (start/no)")
- #           if user_input.lower() in ["start", "no"]:
-  #              bianco = sensor.lux
+i2c = board.I2C()  # uses board.SCL and board.SDA
+sensor = adafruit_tcs34725.TCS34725(i2c)
+user_input = input("calibrazione sensore colori, nero (start/no)")
+if user_input.lower() in ["start", "no"]:
+    nero = sensor.lux
+    print(nero)
+    user_input = input("calibrazione sensore colori, blu (start/no)")
+    if user_input.lower() in ["start", "no"]:
+        blu = sensor.lux
+        print(blu)
+        user_input = input("calibrazione sensore colori, bianco (start/no)")
+        if user_input.lower() in ["start", "no"]:
+            bianco = sensor.lux
+            print(bianco)
 
 ppr = 300.8 
 tstop = 20  
@@ -81,27 +81,26 @@ def send_serial(a): # todo restituisce 1 quando trova nero, 1 se è salita/disce
                 position = (last_AA << 2) | current_aa
                 counter_A += outcome[position]
                 last_AA = current_aa
-                #casella_nera=read_sensor_color_black(nero)
-                #casella_nera=False
-                #if casella_nera:
-                 #   s=str("s")
-                  #  ser.write(s.encode('utf-8'))
-                #    while True:
-                 #       print(current_aa)
-                  #      print(counter_A)
-                   #     current_aa = (left_A << 1) | right_A
-                    #    position = (last_AA << 2) | current_aa
-                     #   counter_A += outcome[position]
-                      #  last_AA = current_aa
-                  #      if(counter_A>0): #serve 1.44 per arrivare a 30cm, cioè un giro completo più 0.44 giri
-                   #         q=str("q")
-                    #        ser.write(q.encode('utf-8'))
-                     #       time.sleep(1)
-                      #      counter_A=0
-                       #     last_AA=0
-                        #    miao="finito"
-                         #   break
-                    #break
+                casella_nera=read_sensor_color_black(nero)
+                if casella_nera:
+                    s=str("s")
+                    ser.write(s.encode('utf-8'))
+                    while True:
+                        print(current_aa)
+                        print(counter_A)
+                        current_aa = (left_A << 1) | right_A
+                        position = (last_AA << 2) | current_aa
+                        counter_A += outcome[position]
+                        last_AA = current_aa
+                        if(counter_A>0): #serve 1.44 per arrivare a 30cm, cioè un giro completo più 0.44 giri
+                            q=str("q")
+                            ser.write(q.encode('utf-8'))
+                            time.sleep(1)
+                            counter_A=0
+                            last_AA=0
+                            miao="finito"
+                            break
+                    break
                 if(counter_A<-2160): #serve 1.44 per arrivare a 30cm, cioè un giro completo più 0.44 giri
                     line = ser.readline().decode("utf-8")
                     print(line)
@@ -193,10 +192,11 @@ def send_serial(a): # todo restituisce 1 quando trova nero, 1 se è salita/disce
    # elif lux >=(specchio-10) and lux <= (specchio+10):
     #    print('specchio')
 
-#def read_sensor_color_black(nero):
- #   lux = sensor.lux
-  #  if lux >=(nero-2) and lux <= (nero+2):
-   #     print('nero')
-    #    return True
-    #else:
-     #   return False
+def read_sensor_color_black(nero):
+    lux = sensor.lux
+    print(lux)
+    if lux >=(nero-2) and lux <= (nero+2):
+        print('nero')
+        return True
+    else:
+        return False
